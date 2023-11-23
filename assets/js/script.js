04db5e00791e8109f72679b156921013a86f9ce5
philip nguyen <nguyen.is.philip@gmail.com>
2023-11-23 11:02:06 -0500

    able to get past present future classes to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,41 +8,33 @@
 
 //Ensures that the script will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
 $(document).ready(function () {
- 
- 
+  // List of IDs
+  let ids = ['hour-9', 'hour-10', 'hour-11', 'hour-12', 'hour-13', 'hour-14', 'hour-15', 'hour-16', 'hour-17'];
 
+  // Get the current dayJS hour
+  let dayjsHour = dayjs().hour();
 
-  //grabs hour from each time slot and compares it to actual time
-  $.each(function () {
-   //get current dayjs hour value
-    let dayjsHour = dayjs().hour();
-    
-    let ids=['hour-9','hour-10','hour-11','hour-12','hour-13','hour-14','hour-15','hour-16','hour-17'];
-    ids.forEach(function(id){
-      let element = document.getElementById(id);
-      let hourNumber= parseInt(id.split('-')[1]);
-      console.log('Original ID:', id);
-      console.log('Number:', hourNumber);
-      
-    // figure out a way to get just the number in the id so that it can be
-    // compared to the dayjshour value
-    if (dayjsHour == hourNumber) {
-      $(this).addClass("present");
-      $(this).children(".description").addClass("white-text");
-    } else if (currentHour < hour) {
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    } else if (currentHour > timeDiv) {
-      $(this).removeClass("future");
-      $(this).addClass("past");
+  // Iterate over each ID
+  ids.forEach(function (id) {
+    // Extract the numeric part from the ID
+    let hourId = parseInt(id.split('-')[1]);
+
+    // Compare the numeric part with the dayJS hour
+    if (hourId < dayjsHour) {
+      $("#" + id).addClass("past");
+    } else if (hourId === dayjsHour) {
+      $("#" + id).addClass("present");
+    } else {
+      $("#" + id).addClass("future");
     }
   });
+});
 
   //grabs values from time and value divs and saves them to local storage
   $(".saveBtn").click(function (event) {
     event.preventDefault();
-    var value = $(this).siblings(".time-block").val();
-    var time = $(this).parent().attr("id").split("-")[1];
+    let value = $(this).siblings(".time-block").val();
+    let time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, value);
     console.log()
   });
@@ -57,5 +49,3 @@ $(document).ready(function () {
   $("#hour-15 .time-block").val(localStorage.getItem("15"));
   $("#hour-16 .time-block").val(localStorage.getItem("16"));
   $("#hour-17 .time-block").val(localStorage.getItem("17"));
-})
-})
